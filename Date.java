99b49bfd634006c77634f99cbe3df70a31f5a7db
fgase <florent.gase@gmail.com>
2014-01-27 10:52:09 +0100

    Create Date.java

---
+++ Date.java
@@ -0,0 +1,67 @@
+
+import java.util.*;
+
+
+/**
+ * Class Date
+ */
+public class Date {
+
+  //
+  // Fields
+  //
+
+  private int jour;
+  private int heure;
+  
+  //
+  // Constructors
+  //
+  public Date () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of jour
+   * @param newVar the new value of jour
+   */
+  private void setJour ( int newVar ) {
+    jour = newVar;
+  }
+
+  /**
+   * Get the value of jour
+   * @return the value of jour
+   */
+  private int getJour ( ) {
+    return jour;
+  }
+
+  /**
+   * Set the value of heure
+   * @param newVar the new value of heure
+   */
+  private void setHeure ( int newVar ) {
+    heure = newVar;
+  }
+
+  /**
+   * Get the value of heure
+   * @return the value of heure
+   */
+  private int getHeure ( ) {
+    return heure;
+  }
+
+  //
+  // Other methods
+  //
+
+}
