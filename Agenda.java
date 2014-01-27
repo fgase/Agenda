3dc03faef82cd64ebb1ab3349beb145c583ff9fe
fgase <florent.gase@gmail.com>
2014-01-27 10:51:18 +0100

    Create Agenda.java

---
+++ Agenda.java
@@ -0,0 +1,50 @@
+
+import java.util.*;
+
+
+/**
+ * Class Agenda
+ */
+public class Agenda {
+
+  //
+  // Fields
+  //
+
+  private List<Rdv> listeRdv;
+  
+  //
+  // Constructors
+  //
+  public Agenda () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of listeRdv
+   * @param newVar the new value of listeRdv
+   */
+  private void setListeRdv ( List<Rdv> newVar ) {
+    listeRdv = newVar;
+  }
+
+  /**
+   * Get the value of listeRdv
+   * @return the value of listeRdv
+   */
+  private List<Rdv> getListeRdv ( ) {
+    return listeRdv;
+  }
+
+  //
+  // Other methods
+  //
+
+}
