e60fca9fe2c5193e2bf4a41c438aadd3fdbdb1e4
fgase <florent.gase@gmail.com>
2014-01-27 10:52:31 +0100

    Create Lieu.java

---
+++ Lieu.java
@@ -0,0 +1,50 @@
+
+import java.util.*;
+
+
+/**
+ * Class Lieu
+ */
+public class Lieu {
+
+  //
+  // Fields
+  //
+
+  private Adresse adresse;
+  
+  //
+  // Constructors
+  //
+  public Lieu () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of adresse
+   * @param newVar the new value of adresse
+   */
+  private void setAdresse ( Adresse newVar ) {
+    adresse = newVar;
+  }
+
+  /**
+   * Get the value of adresse
+   * @return the value of adresse
+   */
+  private Adresse getAdresse ( ) {
+    return adresse;
+  }
+
+  //
+  // Other methods
+  //
+
+}
