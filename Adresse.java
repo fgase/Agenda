6bfd92e3ac2ed4a3342257c2dcd611c017dcc4dc
fgase <florent.gase@gmail.com>
2014-01-27 10:50:58 +0100

    Create Adresse.java

---
+++ Adresse.java
@@ -0,0 +1,48 @@
+
+
+/**
+ * Class Adresse
+ */
+public class Adresse {
+
+  //
+  // Fields
+  //
+
+  private String intitule;
+  
+  //
+  // Constructors
+  //
+  public Adresse () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of intitule
+   * @param newVar the new value of intitule
+   */
+  private void setIntitule ( String newVar ) {
+    intitule = newVar;
+  }
+
+  /**
+   * Get the value of intitule
+   * @return the value of intitule
+   */
+  private String getIntitule ( ) {
+    return intitule;
+  }
+
+  //
+  // Other methods
+  //
+
+}
